20ea5cca96ca084134c989780a5b42339170e4c5
Phildo <Phildo211@gmail.com>
2016-06-29 10:23:17 -0500

    updated comic nodes

--- src/scenes/comicScene.js
+++ src/scenes/comicScene.js
@@ -8,6 +8,7 @@ var ComicScene = function(game, stage)
   var clicker;
 
   var imgs;
+  var nodes;
   var cur_img;
   var prev_btn;
   var next_btn;
@@ -28,24 +29,31 @@ var ComicScene = function(game, stage)
       imgs[i].src = "assets/comic/comic_"+i+".png";
     }
 
-    prev_btn = new ButtonBox(xspace(1)-btn_s/2,node_y-btn_s/2,btn_s,btn_s,function(evt){if(hit_ui) return;cur_img--;hit_ui = true;});
-    next_btn = new ButtonBox(0,0,dc.width,dc.height,                      function(evt){if(hit_ui) return;cur_img++;hit_ui = true;});
+    var xspace = function(n) { return n*(dc.width/(imgs.length+3)); }
+
+    prev_btn = new ButtonBox(xspace(1)            -btn_s/2,node_y-btn_s/2,btn_s,btn_s,function(evt){if(hit_ui) return;cur_img--;hit_ui = true;});
+    next_btn = new ButtonBox(xspace(imgs.length+2)-btn_s/2,node_y-btn_s/2,btn_s,btn_s,function(evt){if(hit_ui) return;cur_img++;hit_ui = true;});
+    full_btn = new ButtonBox(0,0,dc.width,dc.height,                                  function(evt){if(hit_ui) return;cur_img++;hit_ui = true;});
+    nodes = [];
+    for(var i = 0; i < imgs.length; i++)
+    {
+      nodes[i] = new ButtonBox(xspace(i+2)-node_s/2,node_y-btn_s/2,btn_s,btn_s,function(evt){if(hit_ui) return;cur_img = i;hit_ui = true;});
+      clicker.register(nodes[i]);
+    }
     clicker.register(prev_btn);
     clicker.register(next_btn);
+    clicker.register(full_btn);
 
     cur_img = 0;
   };
 
-  var xspace = function(n)
-  {
-    return n*(dc.width/(imgs.length+3));
-  }
 
   var duh = 0;
   self.tick = function()
   {
     if(cur_img >= imgs.length) { game.nextScene(); }
     else clicker.flush();
+    if(cur_img < 0) cur_img = 0;
     hit_ui = false;
   };
 
@@ -58,17 +66,20 @@ var ComicScene = function(game, stage)
       ctx.drawImage(imgs[cur_img],20,20,dc.width-40,dc.height-40);
       ctx.fillStyle = "#FF0000";
       ctx.fillRect(prev_btn.x,prev_btn.y,prev_btn.w,prev_btn.h);
-      ctx.fillRect(xspace(imgs.length+2)-btn_s/2,node_y-btn_s/2,btn_s,btn_s);
+      ctx.fillRect(next_btn.x,next_btn.y,next_btn.w,next_btn.h);
       var x;
       for(var i = 0; i < imgs.length; i++)
       {
-        x = xspace(i+2);
         ctx.fillStyle = "#000000";
-        ctx.fillRect(x-node_s/2,node_y-node_s/2,node_s,node_s);
+        ctx.beginPath();
+        ctx.arc(nodes[i].x+nodes[i].w/2,nodes[i].y+nodes[i].h/2,node_s/2,0,2*Math.PI);
+        ctx.fill();
         if(i == cur_img)
         {
           ctx.fillStyle = "#FFFFFF";
-          ctx.fillRect(x-(node_s*0.8)/2,node_y-(node_s*0.8)/2,(node_s*0.8),(node_s*0.8));
+          ctx.beginPath();
+          ctx.arc(nodes[i].x+nodes[i].w/2,nodes[i].y+nodes[i].h/2,node_s/3,0,2*Math.PI);
+          ctx.fill();
         }
       }
     }
